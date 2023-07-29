16cc8c7574245bcde7fdee5ae945ce546e281ee2
Cristi Sandu <sandu.cristi@yahoo.com>
2023-07-29 16:05:23 +0300

    Add project files.

---
+++ lab6ex2/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6ex2
+{
+    internal class Car
+    {
+        private string marca;
+        private string numarInmatriculare;
+        private int capacitateCilindrica;
+
+        public Car(string marca, string numarInmatriculare, int capacitateCilindrica)
+        {
+            this.marca = marca;
+            this.numarInmatriculare = numarInmatriculare;
+            this.capacitateCilindrica = capacitateCilindrica;
+        }
+
+        public string GetDescription()
+        {
+           return $"{marca}, {numarInmatriculare}, {capacitateCilindrica}cm3";
+
+        }
+    }
+}
---
+++ lab6ex2/Program.cs
@@ -0,0 +1,26 @@
+﻿using lab6ex2;
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /*Scrieti un program care va modela un autoturism. Un autoturism va avea o marca (string), un numar de inmatriculare (string), precum si o capacitate cilindrica(int).
+              Autoturismul va avea:
+               Campurile corespunzatoare caracteristicilor autoturismului;
+                Un constructor care va initializa campurile acestuia;
+               O metoda “Tiparire” care va afisa pe consola descrierea acestuia sub forma : “marca, numarDeInmatriculare, capacitateCilindrica”.
+            */
+
+
+           Car car= new Car ("Dacia","AA_01XYZ",1200);
+
+           Console.WriteLine(car.GetDescription());
+
+        }
+
+    }
+}
